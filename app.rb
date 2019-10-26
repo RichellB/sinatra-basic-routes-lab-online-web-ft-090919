1940e248f64e397c5441335eb245ddd81dda35a2
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-26 22:55:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is "
+    "My name is Richell"
   end
   
   get '/hometown' do
-    "My hometown is "
+    "My hometown is Paterson, New Jersey"
   end
   
   get '/favorite-song' do
-    "My favorite song is "
+    "My favorite song is African Queen"
   end
 
 end
